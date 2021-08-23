c8282d7c56d8b94ba02c74278721cf7e1a982514
brellin <cekakid@gmail.com>
2021-08-23 11:04:04 -0500

    extract projects to js file

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, NavLink } from 'react-router-dom';
 
 import GitHub from 'react-github-calendar';
+import projects from './projects';
 import './Work.scss';
 
 const Work = props => {
@@ -11,62 +12,35 @@ const Work = props => {
             <NavLink exact to='/work'>Projects</NavLink>
             <NavLink to='/work/activity'>Activity</NavLink>
         </div>
-        <Route exact path='/work' component={ Projects } />
+        <Route
+            exact path='/work'
+            component={ _ => projects.map(p => {
+                const [ hovered, setHovered ] = useState(false);
+                return <div
+                    className="Project"
+                    style={ {
+                        backgroundImage: `linear-gradient(#000000${ hovered ? '80' : 'aa' }, #000000${ hovered ? '80' : 'aa' }), url(${ p.img })`
+                    } }
+                    onMouseEnter={ _ => setHovered(true) }
+                    onMouseLeave={ _ => setHovered(false) }
+                >
+                    <h2>{ p.title }</h2>
+                    <p onClick={ _ => window.open(p.url) }>
+                        { p.description + '\n\n' }
+                        <a
+                            target='_blank'
+                            rel='noopener noreferrer'
+                            href={ p.github }
+                            onClick={ e => e.stopPropagation() }
+                        >Github Repository</a></p>
+                </div>;
+            }) } />
         <Route path='/work/activity' component={ Activity } />
     </div>;
 
 };
 
-const Projects = props => {
-    return <>
-        <div className="Project BlackHole">
-            <h2>Black Hole</h2>
-
-            <p onClick={ () => window.open('https://blackhole.willujr.com') }>{ `A web app using React for the front-end and Node for the back-end. This project allows the user to vent into a “black hole” - without the worry of anybody hearing what was said. 
-            
-            I was responsible for the front end of this project.
-            
-            `}<a
-                    href="https://github.com/black-hole-lambda-build-week/front-end"
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    onClick={ e => e.stopPropagation() }
-                >GitHub Repository</a></p>
-        </div>
-
-        <div className="Project Howl">
-            <h2>Howl For Change</h2>
-
-            <p onClick={ () => window.open('https://howl-for-change-staging.netlify.com') }>{ `A marketplace using React for the front-end, Firebase for the backend, and Node to interact with Dwolla for payments. Allows nonprofit organizations to post projects for creative users to apply for - using portfolios made within the app. Only one 'winner' is chosen per project.
-            
-            I was the lead technical developer for this project.
-            
-            `}<a
-                    href="https://bitbucket.org/hfc-wolfpack/howlforchange/"
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    onClick={ e => e.stopPropagation() }
-                >Bitbucket Repository</a></p>
-        </div>
-
-        <div className="Project Chao">
-            <h2>Chao Fever</h2>
-
-            <p onClick={ () => window.open('https://chaofever.willujr.com') }>{ `My first maserpiece, made at 15 years of age - 2008. Originally composed using HTML, CSS, and PHP. This iteration was refactored to use HTML and LESS.
-            
-            The website is focused on a side-story of the Sonic Adventure series...Chao!
-            
-            `}<a
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    href="https://github.com/brellin/chaofever"
-                    onClick={ e => e.stopPropagation() }
-                >GitHub Repository</a></p>
-        </div>
-    </>;
-};
-
-const Activity = props => {
+const Activity = _ => {
     return <>
         <h2>WakaTime</h2>
         <figure><embed src="https://wakatime.com/share/@brellin/f844d8ee-85dc-4ba1-b3e1-4277288ef657.svg"></embed></figure>

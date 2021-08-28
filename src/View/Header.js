0278ef5d215de9472f56f8e6b3b1271e0d34b8e6
brellin <cekakid@gmail.com>
2021-08-28 13:00:03 -0500

    change 'resume' styles followed

--- src/View/Header.js
+++ src/View/Header.js
@@ -7,11 +7,7 @@ const Header = props => {
         <NavLink exact to='/' className='home'>Home</NavLink>
         <NavLink to='/about' className='about'>About</NavLink>
         <NavLink to='/work' className='work'>Work</NavLink>
-        <a
-            href="https://docs.google.com/document/d/1jRMkE040orW6gBoYssTHJ7g8s8VN3OPtYTF-fz_EYII/edit?usp=sharing"
-            target='_blank'
-            rel='noopener noreferrer'
-        >Resume</a>
+        <NavLink to='/blog' className='blog'>Blog</NavLink>
         <hr />
     </nav>;
 

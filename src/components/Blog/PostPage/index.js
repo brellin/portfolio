c69d972a7776997a33f020f99473a6dd8f1fc454
brellin <cekakid@gmail.com>
2021-08-28 14:06:30 -0500

    better auth

--- src/components/Blog/PostPage/index.js
+++ src/components/Blog/PostPage/index.js
@@ -11,22 +11,7 @@ const PostPage = ({ edit, newPost }) => {
     const { push } = useHistory();
 
     const [ post, setPost ] = useState({ title: '', text: '', name: '', date: '' });
-    const [ isWill, setIsWill ] = useState(false);
     const [ proof, setProof ] = useState({});
-
-    useEffect(_ => {
-
-        if (proof.password && proof.name) {
-            axios.defaults.headers = proof;
-            axios.post('/auth')
-                .then(res => res.status === (200 || 204) && setIsWill(true))
-                .catch(err => {
-                    console.error(err);
-                    push('/wrong');
-                });
-        }
-
-    }, [ proof ]);
 
     useEffect(_ => {
         if (!newPost) axios.get(`/posts/${ id }`)
@@ -34,45 +19,70 @@ const PostPage = ({ edit, newPost }) => {
             .catch(err => console.error(err));
     }, [ id ]);
 
-    return <div className='Post'>
-        { (edit || newPost) ?
+    if (edit || newPost) {
+
+        useEffect(_ => {
+
+            if (proof.password && proof.name) {
+                axios.defaults.headers = proof;
+                axios.post('/auth')
+                    .then(res => res.status === (200 || 204) && setIsWill(true))
+                    .catch(err => {
+                        console.error(err);
+                        push('/wrong');
+                    });
+            }
+
+        }, [ proof ]);
+
+        const [ isWill, setIsWill ] = useState(false);
+
+        const sendData = _ => edit ? axios
+
+            .put(`/posts/${ id }`, post)
+            .then(r => {
+                console.log(r);
+                push(`/blog/post/${ id }`);
+            })
+
+            .catch(err => console.error(err))
+            :
+            axios
+                .post('/posts/new', post)
+                .then(r => {
+                    console.log(r);
+                    push(`/blog/post/${ r.data.id }`);
+                })
+
+                .catch(err => console.error(err));
+
+        return <div className="Post">
+            { !isWill ? <div className="Restricted">
+                <div className="Verification">
+                    <button onClick={ _ => openFileDialog(setProof) }>CHECK YOURSELF</button>
+                </div>
+            </div> : null }
+
             <input
                 type="text"
                 value={ post.title }
                 onChange={ e => setPost({ ...post, title: e.target.value }) }
             />
-            :
-            <h2>{ post.title }</h2> }
-        <span>{ `${ post.name } on ${ moment(post.date, 'x').format('LL') }${ (edit || post.edited) ? ` (last edited on ${ moment(edit ? Date.now() : post.edited, 'x').format('LL') })` : '' }` }</span>
-        { (edit || newPost) ?
+            <span>{ `${ 'Name' } on ${ moment(post.date || Date.now().toString(), 'x').format('LL') }${ (edit || post.edited) ? ` (last edited on ${ moment(edit ? Date.now() : post.edited, 'x').format('LL') })` : '' }` }</span>
             <textarea
                 value={ post.text }
                 onChange={ e => setPost({ ...post, text: e.target.value }) }
             />
-            :
-            <p>{ post.text }</p> }
+            { isWill ?
+                <button onClick={ sendData }>Submit</button>
+                : null }
+        </div>;
+    }
 
-        { (edit || newPost) ? !isWill ? <button onClick={ _ => openFileDialog(setProof) }>Verify</button>
-            :
-            <button onClick={ _ => edit ? axios
-
-                .put(`/posts/${ id }`, post)
-                .then(r => {
-                    console.log(r);
-                    push(`/blog/post/${ id }`);
-                })
-
-                .catch(err => console.error(err))
-                :
-                axios
-                    .post('/posts/new', post)
-                    .then(r => {
-                        console.log(r);
-                        push(`/blog/post/${ r.data.id }`);
-                    })
-
-                    .catch(err => console.error(err))
-            }>Submit</button> : null }
+    return <div className='Post'>
+        <h2>{ post.title }</h2>
+        <span>{ `${ post.name } on ${ moment(post.date, 'x').format('LL') }${ (edit || post.edited) ? ` (last edited on ${ moment(edit ? Date.now() : post.edited, 'x').format('LL') })` : '' }` }</span>
+        <p>{ post.text }</p>
     </div>;
 
 };

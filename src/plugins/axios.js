ccd48905876a19eaab7cde3d288f960b52dbcde1
brellin <cekakid@gmail.com>
2022-07-14 23:42:48 -0500

    filtering working with backend

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://pfl-be.herokuapp.com';
+axios.defaults.baseURL = location.hostname === 'localhost' ? 'http://localhost:5000' : 'https://pfl-be.herokuapp.com';
 
 export default axios;
--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,5 +1,10 @@
 <template>
   <main>
+    <h3>Filter</h3>
+    <select @change="updateCategory">
+      <option value="">select a category</option>
+      <option v-for="c, i in categories" :key="i" :value="c">{{c}}</option>
+    </select>
     <div :class="`post-wall ${posts.length === 0 ? 'pulse' : ''}`">
       <Post v-for="p in posts" :key="p.id" v-bind="p" :loading="loading" />
     </div>
@@ -13,19 +18,35 @@ export default {
   name: "wu-blog",
   data() {
     return {
-      posts: [{}, {}, {}],
+      posts: [ {}, {}, {} ],
+      categories: [ {} ],
       loading: true,
     };
   },
   mounted() {
     axios
-      .get("/posts")
+      .get("/posts/categories")
       .then(({ data }) => {
-        this.posts = data;
-        this.loading = false;
+        this.categories = data;
+        axios
+          .get("/posts/category/general")
+          .then(({ data }) => {
+          this.posts = data
+          this.loading = false
+        })
       })
       .catch((err) => console.error(err));
   },
+  methods: {
+    updateCategory(e) {
+      const cat = e.target.value
+      console.log(cat)
+      axios.get(`/posts/category/${ cat?cat:'general' }`)
+        .then(({ data }) => {
+      this.posts = data
+    })
+  }
+},
   components: {
     Post,
   },
@@ -42,6 +63,10 @@ main {
 
   h1 {
     font-size: 8rem;
+  }
+
+  select, option {
+    text-transform: capitalize;
   }
 
   div.post-wall {

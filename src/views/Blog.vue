f6f7309fd6beeda615dbc571503ccc401c2892e9
brellin <cekakid@gmail.com>
2022-08-18 21:04:08 -0500

    use datalist in edit postpage lint in blog and jsconfig preserve jsx

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -4,106 +4,103 @@
       <h2>Filter</h2>
       <select @change="updateCategory">
         <option value="">all categories</option>
-        <option v-for="                         c, i                          in categories" :key="i" :value="c">{{ c }}
-        </option>
+        <option v-for="(c, i) in categories" :key="i" :value="c">{{ c }}</option>
       </select>
     </div>
-    <div :class="`post-wall ${ posts.length === 0 ? 'pulse' : '' }`">
-      <Post v-for="                    p                     in posts" :key="p.id" v-bind="p" :loading="loading" />
+    <div :class="`post-wall ${posts.length === 0 ? 'pulse' : ''}`">
+      <Post v-for="p in posts" :key="p.id" v-bind="p" :loading="loading" />
     </div>
   </main>
 </template>
 
 <script>
-import axios from "../plugins/axios";
-import Post from "../components/Post.vue";
-export default {
-  name: "wu-blog",
-  data() {
-    return {
-      posts: [ {}, {}, {} ],
-      loading: true,
-    };
-  },
-  mounted() {
-    axios
-      .get("/posts/categories")
-      .then(({ data }) => {
-        this.$store.commit('populateCategories', data);
-        axios
-          .get("/posts")
-          .then(({ data }) => {
+  import axios from "../plugins/axios";
+  import Post from "../components/Post.vue";
+  export default {
+    name: "wu-blog",
+    data() {
+      return {
+        posts: [{}, {}, {}],
+        loading: true,
+      };
+    },
+    mounted() {
+      axios
+        .get("/posts/categories")
+        .then(({ data }) => {
+          this.$store.commit("populateCategories", data);
+          axios.get("/posts").then(({ data }) => {
             this.posts = data;
             this.loading = false;
           });
-      })
-      .catch((err) => console.error(err));
-  },
-  methods: {
-    updateCategory(e) {
-      const cat = e.target.value;
-      axios.get(cat ? `/posts/category/${ cat }` : '/posts')
-        .then(({ data }) => {
+        })
+        .catch((err) => console.error(err));
+    },
+    methods: {
+      updateCategory(e) {
+        const cat = e.target.value;
+        axios.get(cat ? `/posts/category/${cat}` : "/posts").then(({ data }) => {
           this.posts = data;
         });
-      this.size = 1;
-      this.blur();
-    }
-  },
-  components: {
-    Post,
-  },
-  computed: {
-    categories() { return this.$store.state.categories; }
-  }
-};
+        this.size = 1;
+        this.blur();
+      },
+    },
+    components: {
+      Post,
+    },
+    computed: {
+      categories() {
+        return this.$store.state.categories;
+      },
+    },
+  };
 </script>
 
 <style lang="scss" scoped>
-main {
-  @include flex(column, flex-start, center);
+  main {
+    @include flex(column, flex-start, center);
 
-  @media (max-width: 500px) {
-    margin-top: -25px;
-  }
-
-  h1 {
-    font-size: 8rem;
-  }
-
-  div.filter {
-    border: 1px solid black;
-    border-radius: 5px;
-    padding: 10px 5px 5px;
-    position: relative;
-    background: $text-bg;
-
-    h2 {
-      position: absolute;
-      top: -22.5px;
-      background: linear-gradient($light 53%, transparent 50%);
+    @media (max-width: 500px) {
+      margin-top: -25px;
     }
 
-    select,
-    option {
-      text-transform: capitalize;
-      outline: none;
+    h1 {
+      font-size: 8rem;
     }
 
-    select {
-      background: none;
-      border: none;
+    div.filter {
+      border: 1px solid black;
+      border-radius: 5px;
+      padding: 10px 5px 5px;
+      position: relative;
+      background: $text-bg;
 
+      h2 {
+        position: absolute;
+        top: -22.5px;
+        background: linear-gradient($light 53%, transparent 50%);
+      }
+
+      select,
       option {
-        background: $text-bg;
+        text-transform: capitalize;
+        outline: none;
+      }
+
+      select {
+        background: none;
+        border: none;
+
+        option {
+          background: $text-bg;
+        }
       }
     }
 
+    div.post-wall {
+      width: 100%;
+      @include flex(column, flex-start, center);
+    }
   }
-
-  div.post-wall {
-    width: 100%;
-    @include flex(column, flex-start, center);
-  }
-}
 </style>
--- src/views/PostPage.vue
+++ src/views/PostPage.vue
@@ -10,210 +10,229 @@
 
     <div class="category" v-if="auth">
       <h3>Category</h3>
-      <input type="text" name="category" @input="handleCat" :value="post.category">
-      <select name="category" @change="handleChange">
+      <input
+        list="categories"
+        type="text"
+        name="category"
+        @input="handleCat"
+        :value="post.category"
+      />
+      <datalist id="categories" name="category" @change="handleChange">
         <option value=""></option>
-        <option v-for="                c, i                 in cats" :value="c" :key="i">
+        <option v-for="(c, i) in cats" :value="c" :key="i">
           {{ c }}
         </option>
-      </select>
+      </datalist>
     </div>
 
     <h2 v-if="!auth">{{ post.title }}</h2>
-    <input v-else name="title" :value="post.title" placeholder="Title" type="text" @change="handleChange" />
+    <input
+      v-else
+      name="title"
+      :value="post.title"
+      placeholder="Title"
+      type="text"
+      @change="handleChange"
+    />
 
     <span>{{ editedString(post.date, newPost ? null : post.edited) }}</span>
 
     <p v-if="!auth">{{ post.text }}</p>
-    <textarea v-else :value="post.text"
+    <textarea
+      v-else
+      :value="post.text"
       placeholder="Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, iste. Dignissimos perferendis iusto molestias totam animi beatae odio aliquam ut minima nulla, doloribus quae natus fuga, voluptatibus illo! Cupiditate, commodi."
-      @change="handleChange" name="text" />
+      @change="handleChange"
+      name="text"
+    />
 
     <button v-if="auth" @click="submitPost">Submit</button>
   </main>
 </template>
 
 <script>
-import Modal from "../components/Modal.vue";
-import axios from "../plugins/axios";
-import { momentize, editedString } from "../assets/functions";
-export default {
-  name: "post-page-wu",
-  components: {
-    Modal,
-  },
-  methods: {
-    editedString,
-    momentize,
-    goBack() {
-      this.$router.push("/blog");
+  import Modal from "../components/Modal.vue";
+  import axios from "../plugins/axios";
+  import { momentize, editedString } from "../assets/functions";
+  export default {
+    name: "post-page-wu",
+    components: {
+      Modal,
     },
-    editPost() {
-      this.$router.push(`${ this.$route.path }/edit`);
-    },
-    submitPost() {
-      this.newPost
-        ? axios
-          .post("/posts/new", this.post)
-          .then((r) => {
-            console.log(r);
-            this.$router.push(`/blog/post/${ r.data.id }`);
-          })
-
-          .catch((err) => console.error(err))
-        : axios
-          .put(`/posts/${ this.post.id }`, this.post)
-          .then((r) => {
-            console.log(r);
-            this.$router.push(`/blog/post/${ this.post.id }`);
-          })
-
-          .catch((err) => console.error(err));
-    },
-    handleChange(e) {
-      this.post[ e.target.name ] = e.target.value;
-    },
-    handleCat(e) {
-      const catName = e.target.value;
-      this.handleChange(e);
-      this.cats = this.categories.filter(cat => new RegExp(catName, 'i').test(cat));
-    },
-    deletePost() {
-      window.confirm(`Do you really want to delete ${ this.post.title }?`)
-        ? axios
-          .delete(`/posts/${ this.post.id }`)
-          .then(() => this.$router.push("/blog"))
-          .catch((err) => console.error(err))
-        : alert("You have chosen not to delete the post.");
-    },
-  },
-  data() {
-    return {
-      post: {
-        text: "",
-        date: Date.now().toString(),
-        edited: Date.now().toString(),
-        title: "",
-        category: ""
+    methods: {
+      editedString,
+      momentize,
+      goBack() {
+        this.$router.push("/blog");
       },
-      loading: true,
-      cats: this.$store.state.categories
-    };
-  },
-  mounted() {
-    if (this.ending !== "new")
-      axios
-        .get(`/posts/${ this.$route.params.id }`)
-        .then(({ data }) => {
-          this.post = data;
-          this.loading = false;
-        })
-        .catch((err) => console.error(err));
-    else this.loading = false;
-  },
-  computed: {
-    ending() {
-      return this.$route.path.split("/").pop();
+      editPost() {
+        this.$router.push(`${this.$route.path}/edit`);
+      },
+      submitPost() {
+        this.newPost
+          ? axios
+              .post("/posts/new", this.post)
+              .then((r) => {
+                console.log(r);
+                this.$router.push(`/blog/post/${r.data.id}`);
+              })
+
+              .catch((err) => console.error(err))
+          : axios
+              .put(`/posts/${this.post.id}`, this.post)
+              .then((r) => {
+                console.log(r);
+                this.$router.push(`/blog/post/${this.post.id}`);
+              })
+
+              .catch((err) => console.error(err));
+      },
+      handleChange(e) {
+        this.post[e.target.name] = e.target.value;
+      },
+      handleCat(e) {
+        const catName = e.target.value;
+        this.handleChange(e);
+        this.cats = this.categories.filter((cat) => new RegExp(catName, "i").test(cat));
+      },
+      deletePost() {
+        window.confirm(`Do you really want to delete ${this.post.title}?`)
+          ? axios
+              .delete(`/posts/${this.post.id}`)
+              .then(() => this.$router.push("/blog"))
+              .catch((err) => console.error(err))
+          : alert("You have chosen not to delete the post.");
+      },
     },
-    auth() {
-      return /new|edit/i.test(this.ending);
+    data() {
+      return {
+        post: {
+          text: "",
+          date: Date.now().toString(),
+          edited: Date.now().toString(),
+          title: "",
+          category: "",
+        },
+        loading: true,
+        cats: this.$store.state.categories,
+      };
     },
-    newPost() {
-      return this.ending === "new";
+    mounted() {
+      if (this.ending !== "new")
+        axios
+          .get(`/posts/${this.$route.params.id}`)
+          .then(({ data }) => {
+            this.post = data;
+            this.loading = false;
+          })
+          .catch((err) => console.error(err));
+      else this.loading = false;
     },
-    isWill() {
-      return this.$store.state.isWill;
+    computed: {
+      ending() {
+        return this.$route.path.split("/").pop();
+      },
+      auth() {
+        return /new|edit/i.test(this.ending);
+      },
+      newPost() {
+        return this.ending === "new";
+      },
+      isWill() {
+        return this.$store.state.isWill;
+      },
+      categories() {
+        return this.$store.state.categories;
+      },
     },
-    categories() {
-      return this.$store.state.categories;
-    }
-  },
-};
+  };
 </script>
 
-<style lang='scss' scoped>
-main {
-  width: 100%;
-  margin: 50px auto 0;
-  @include flex(column, center, center, wrap);
-  background: $text-bg;
-  box-shadow: 0 0 250px 15px $text-bg;
-
-  button {
-    @include button();
-    align-self: flex-start;
-    margin-top: -35px;
-    margin-bottom: 35px;
-  }
-
-  div.buttons {
-    @include flex($justify: space-between);
+<style lang="scss" scoped>
+  main {
     width: 100%;
-  }
-
-  h2 {
-    margin: 0;
-    font-size: 4rem;
-  }
-
-  span {
-    font-size: 1.3rem;
-    color: slategray;
-  }
-
-  p {
-    font-size: 2rem;
-    white-space: pre-line;
-  }
-
-  input,
-  textarea {
-    width: 100%;
+    margin: 50px auto 0;
+    padding: 0 25px;
     box-sizing: border-box;
-    border-radius: 5px;
-  }
+    @include flex(column, center, center, wrap);
+    background: $text-bg;
+    box-shadow: 0 0 250px 15px $text-bg;
 
-  input {
-    font-size: 4rem;
-    padding: 0;
-    text-align: center;
-  }
+    button {
+      @include button();
+      align-self: flex-start;
+      margin-top: -35px;
+      margin-bottom: 35px;
+    }
 
-  textarea {
-    min-height: 200px;
-    resize: none;
-    padding: 0 15px;
-  }
+    div.buttons {
+      @include flex($justify: space-between);
+      width: 100%;
+    }
 
-  button {
-    @include button(3rem);
-    margin-top: 5px;
-  }
+    h2 {
+      margin: 0;
+      font-size: 4rem;
+    }
 
-  div.category {
-    @include flex(column, center, center);
-    background: field;
-    border: 1px solid black;
-    position: relative;
-    padding: 15px 0 5px;
-    margin-bottom: 15px;
+    span {
+      font-size: 1.3rem;
+      color: slategray;
+    }
 
-    h3 {
-      font-size: 1.75rem;
-      background: linear-gradient(transparent 41%, field 41%);
-      position: absolute;
-      top: -27.5px;
-      left: 5px;
+    p {
+      font-size: 2rem;
+      white-space: pre-line;
     }
 
     input,
-    select {
-      outline: none;
-      background: none;
-      border: none;
-      font-size: 2rem;
-      text-transform: capitalize;
+    textarea {
+      width: 100%;
+      box-sizing: border-box;
+      border-radius: 5px;
+    }
+
+    input {
+      font-size: 4rem;
+      padding: 0;
+      text-align: center;
+    }
+
+    textarea {
+      min-height: 200px;
+      resize: none;
+      padding: 0 15px;
+    }
+
+    button {
+      @include button(3rem);
+      margin-top: 5px;
+    }
+
+    div.category {
+      @include flex(column, center, center);
+      background: field;
+      border: 1px solid black;
+      position: relative;
+      padding: 15px 0 5px;
+      margin-bottom: 15px;
+
+      h3 {
+        font-size: 1.75rem;
+        background: linear-gradient(transparent 41%, field 41%);
+        position: absolute;
+        top: -27.5px;
+        left: 5px;
+      }
+
+      input,
+      datalist {
+        outline: none;
+        background: none;
+        border: none;
+        font-size: 2rem;
+        text-transform: capitalize;
+      }
     }
   }
-}
 </style>
